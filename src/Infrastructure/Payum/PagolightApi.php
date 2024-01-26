a29f7cf461af6290ee1fe71e8ddbc30a33eade9e
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2024-01-26 15:44:52 +0100

    Remove duplicated constants

--- src/Infrastructure/Payum/PagolightApi.php
+++ src/Infrastructure/Payum/PagolightApi.php
@@ -6,20 +6,11 @@ namespace Webgriffe\SyliusPagolightPlugin\Infrastructure\Payum;
 
 final class PagolightApi
 {
-    private const API_URL_TEST = 'https://sbx-origination.heidipay.io';
-
-    private const API_URL_LIVE = 'https://origination.heidipay.com';
-
     /**
      * @param array{sandbox: bool, merchant_key: string} $config
      */
     public function __construct(private readonly array $config)
     {
-    }
-
-    public function getApiEndpoint(): string
-    {
-        return $this->config['sandbox'] ? self::API_URL_TEST : self::API_URL_LIVE;
     }
 
     public function getMerchantKey(): string
