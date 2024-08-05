0425625707cfa867caa3936247d2a2cfd9a803d1
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2024-08-05 11:27:30 +0200

    Another fix for routing

--- config/shop_ajax_routing.php
+++ config/shop_ajax_routing.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routes): void {
-    $routes->add('webgriffe_sylius_pagolight_plugin_payment_status', '/payment/{paymentId}/status')
+    $routes->add('webgriffe_sylius_pagolight_plugin_payment_status', '/payment/{paymentId}/pagolight-status')
         ->controller(['webgriffe_sylius_pagolight.controller.payment', 'statusAction'])
         ->methods(['GET'])
         ->requirements(['paymentId' => '\d+'])
