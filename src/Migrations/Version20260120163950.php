aaa254aebb4b4429a0a4e81f483d6ab3a42a040d
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2026-01-20 17:56:26 +0100

    Fix Psalm

--- src/Migrations/Version20260120163950.php
+++ src/Migrations/Version20260120163950.php
@@ -9,11 +9,13 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20260120163950 extends AbstractMigration
 {
+    #[\Override]
     public function getDescription(): string
     {
         return 'Create webgriffe_sylius_heylight_webhook_token table if not exists';
     }
 
+    #[\Override]
     public function up(Schema $schema): void
     {
         if ($schema->hasTable('webgriffe_sylius_heylight_webhook_token')) {
@@ -23,6 +25,7 @@ final class Version20260120163950 extends AbstractMigration
         $this->addSql('ALTER TABLE webgriffe_sylius_heylight_webhook_token ADD CONSTRAINT FK_EA20794D4C3A3BB FOREIGN KEY (payment_id) REFERENCES sylius_payment (id) ON DELETE CASCADE');
     }
 
+    #[\Override]
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE webgriffe_sylius_heylight_webhook_token DROP FOREIGN KEY FK_EA20794D4C3A3BB');
