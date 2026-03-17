c4189b82754366c23f2a8d0a09da48bff4bd35c0
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2026-03-17 16:34:14 +0100

    Fix admin config

--- config/packages/sylius_twig_hooks.php
+++ config/packages/sylius_twig_hooks.php
@@ -5,36 +5,26 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 return static function (ContainerConfigurator $container): void {
+    $configFields = [
+        'merchant_key' => [
+            'template' => '@WebgriffeSyliusHeylightPlugin/admin/payment_method/form/merchant_key.html.twig',
+            'priority' => 0,
+        ],
+        'allowed_terms' => [
+            'template' => '@WebgriffeSyliusHeylightPlugin/admin/payment_method/form/allowed_terms.html.twig',
+            'priority' => 0,
+        ],
+        'sandbox' => [
+            'template' => '@WebgriffeSyliusHeylightPlugin/admin/payment_method/form/sandbox.html.twig',
+            'priority' => 0,
+        ],
+    ];
     $container->extension('sylius_twig_hooks', [
         'hooks' => [
-            'sylius_admin.payment_method.create.content.form.sections.gateway_configuration' => [
-                'merchant_key' => [
-                    'template' => '@WebgriffeSyliusHeylightPlugin/admin/payment_method/form/merchant_key.html.twig',
-                    'priority' => 0,
-                ],
-                'allowed_terms' => [
-                    'template' => '@WebgriffeSyliusHeylightPlugin/admin/payment_method/form/allowed_terms.html.twig',
-                    'priority' => 0,
-                ],
-                'sandbox' => [
-                    'template' => '@WebgriffeSyliusHeylightPlugin/admin/payment_method/form/sandbox.html.twig',
-                    'priority' => 0,
-                ],
-            ],
-            'sylius_admin.payment_method.update.content.form.sections.gateway_configuration' => [
-                'merchant_key' => [
-                    'template' => '@WebgriffeSyliusHeylightPlugin/admin/payment_method/form/merchant_key.html.twig',
-                    'priority' => 0,
-                ],
-                'allowed_terms' => [
-                    'template' => '@WebgriffeSyliusHeylightPlugin/admin/payment_method/form/allowed_terms.html.twig',
-                    'priority' => 0,
-                ],
-                'sandbox' => [
-                    'template' => '@WebgriffeSyliusHeylightPlugin/admin/payment_method/form/sandbox.html.twig',
-                    'priority' => 0,
-                ],
-            ],
+            'sylius_admin.payment_method.create.content.form.sections.gateway_configuration.heylight_bnpl' => $configFields,
+            'sylius_admin.payment_method.update.content.form.sections.gateway_configuration.heylight_bnpl' => $configFields,
+            'sylius_admin.payment_method.create.content.form.sections.gateway_configuration.heylight_financing' => $configFields,
+            'sylius_admin.payment_method.update.content.form.sections.gateway_configuration.heylight_financing' => $configFields,
             'webgriffe_sylius_heylight.payment.process' => [
                 'content' => [
                     'template' => '@WebgriffeSyliusHeylightPlugin/shop/payment/process/content.html.twig',
